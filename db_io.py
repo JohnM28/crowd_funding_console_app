a6e1da03e20087d1606149afc2c46df5df1c6b97
John <johnmagdy@rocketmail.com>
2022-01-29 02:48:30 +0200

    completed_app

--- db_io.py
+++ db_io.py
@@ -4,19 +4,32 @@ with sqlite3.connect('useres.db') as conn:
     cur = conn.cursor()
 
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS user_info (id integer PRIMARY KEY, f_name TEXT, l_name TEXT, email TEXT, password TEXT,phone TEXT);"
+    "CREATE TABLE IF NOT EXISTS user_info2 (email TEXT PRIMARY KEY, f_name TEXT, l_name TEXT, password TEXT,phone TEXT);"
 )
 
-def add_info(id,f_name, l_name, email, passwd, phone):
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS project_info2 (id integer PRIMARY KEY, title TEXT,details TEXT, target integer, st_date TEXT, t_date TEXT , email TEXT ,FOREIGN KEY (email) references user_info2 (email) );"
+)
+
+def add_user(email, f_name, l_name, passwd, phone):
         cur.execute(
-            "INSERT INTO user_info VALUES (?,?,?,?,?,?);",
-            (id,f_name, l_name, email, passwd, phone, )
+            "INSERT INTO user_info2 VALUES (?,?,?,?,?);",
+            (email, f_name, l_name, passwd, phone,)
         )
         conn.commit()
+
+def add_project(id, title, details, target, st_date,t_date,email):
+    cur.execute(
+        "INSERT INTO project_info2 VALUES (?,?,?,?,?,?,?);",
+        (id, title, details, target, st_date,t_date,email,)
+    )
+    conn.commit()
+
+
 def get_email(email):
     try:
         cur.execute(
-            "SELECT email FROM user_info where email = ?",
+            "SELECT email FROM user_info2 where email = ?",
             (email, )
         )
         ret_email = cur.fetchall()[0][0]
@@ -28,7 +41,7 @@ def get_email(email):
 def get_passwd(passwd):
     try:
         cur.execute(
-            "SELECT password FROM user_info where password = ?",
+            "SELECT password FROM user_info2 where password = ?",
             (passwd, )
         )
         ret_passwd = cur.fetchall()[0][0]
@@ -38,3 +51,39 @@ def get_passwd(passwd):
     except Exception:
         pass
 
+def max_id():
+    cur.execute(
+        "SELECT * FROM project_info2 order by id desc limit 1",
+    )
+    ret_id = cur.fetchall()[0][0]
+    print(ret_id)
+    return ret_id
+
+def get_projects():
+    for row in cur.execute(
+        "SELECT * FROM project_info2"
+    ):
+        print (f'Title: {row[1]}. \nDescription: {row[2]}. \nTarget: {row[3]}EGP\n\n')
+
+def get_user_projects(email):
+    for row in cur.execute(
+        "SELECT * FROM project_info2 where email = ?",
+            (email,)
+    ):
+        print (f'Title: {row[1]}. \nDescription: {row[2]}. \nTarget: {row[3]}EGP\n\n')
+
+def delete_table(email, title):
+    try:
+        cur.execute(
+            "SELECT email FROM user_info2 where email = ? AND title = ?",
+            (email,title,)
+        )
+        print("Deleted Successfully")
+    except Exception:
+        print("no project to delete")
+
+# def udpate_table(email, title):
+#     try:
+#         print("Updated Successfully")
+#     except Exception:
+#         print("no project to update")
--- main.py
+++ main.py
@@ -1,59 +1,6 @@
 import db_io
 
-class register:
-    # Constructor
-    id = 4
-    def __init__(self, f_name, l_name, email, passwd, phone):
-        self.f_name = f_name
-        self.l_name = l_name
-        self.email = email
-        self.passwd = passwd
-        self.phone = phone
-        self.id = register.id
-        db_io.add_info(self.id,self.f_name, self.l_name, self.email, self.passwd, self.phone)
-        register.id = +1
-
-    @classmethod
-    def reg(cls):
-        return cls(
-            input("first name: "),
-            input("last name: "),
-            input("email: "),
-            input("password: "),
-            input("phone: "),
-        )
-
-    # @staticmethod
-    # def login(email, passwd):
-    #     while True:
-    #         email = input("email: ")
-    #         if email != "john":
-    #             pass
-    #         passwd = input("password: ")
-    # def check_budget(self, budget):
-    #     # Check if the budget is valid
-    #     if not isinstance(budget, (int, float)):
-    #         print('Enter float or int')
-    #         exit()
-    #     if budget < 0:
-    #         print('Sorry you don\'t have money')
-    #         exit()
-    #
-    # def get_change(self, budget):
-    #     return budget - self.price
-    #
-    # def sell(self, budget):
-    #     self.check_budget(budget)
-    #     if budget >= self.price:
-    #         print(f'You can buy the {self.name} coffee')
-    #         if budget == self.price:
-    #             print('It\'s complete')
-    #         else:
-    #             print(f'Here is your change {self.get_change(budget)}$')
-    #
-    #         exit('Thanks for your transaction')
-
-class login:
+class Login():
     def __init__(self, email, passwd):
         self.email = email
         self.passwd = passwd
@@ -66,42 +13,108 @@ class login:
     def check_cerd(self):
         print(self.email +" this is email")
         if self.email != db_io.get_email(self.email):
-            print("no such email exists")
+            print("ERROR no such email exists \n")
             print(self.email)
+            return False
         if self.passwd != db_io.get_passwd(self.passwd):
-            print("password doesnt match")
+            print("ERROR password doesnt match \n")
             print(self.passwd)
+            return False
+        else:
+            return True
     def log(self):
         print(self.passwd)
         self.check_cerd()
 
 
-class interface(login):
-    def __init__(self, email = None, passwd = None):
-        print ("foobar init")
-        login.__init__(self, email, passwd)
-        print(self.email)
-        print(self.passwd)
+class Register:
+    # Constructor
+    # id = 1
+    def __init__(self, email, f_name, l_name,  passwd, phone):
+        self.email = email
+        self.f_name = f_name
+        self.l_name = l_name
+        self.passwd = passwd
+        self.phone = phone
+        # self.id = Register.id
+        db_io.add_user(self.email, self.f_name, self.l_name, self.passwd, self.phone)
+        # Register.id = +1
+
+    @classmethod
+    def reg(cls):
+        return cls(
+            input("email: "),
+            input("first name: "),
+            input("last name: "),
+            input("password: "),
+            input("phone: "),
+        )
+
+
+
+class Projects():
+    id = db_io.max_id()
+    def __init__(self,title= None, details= None, target= None, st_time= None, e_time= None, email = None):
+        self.title = title
+        self.details = details
+        self.target = target
+        self.st_time = st_time
+        self.e_time = e_time
+        self.email = email
+        self.id = Projects.id
+        db_io.add_project(self.id, self.title, self.details, self.target, self.st_time, self.e_time, self.email)
+        Projects.id = +1
+
+    @classmethod
+    def prog(cls, email):
+        return cls(
+            input("Title: "),
+            input("Details: "),
+            input("Target: "),
+            input("Start 'use a dd-mm-yy formula': "),
+            input("End: 'use a dd-mm-yy formula': "),
+            email,
+        )
+
+
+class Interface():
     def start_page(self):
-        nav = input(print("to register press 1 \n to login press 2\n"))
-        if nav == "1":
-            register.reg()
-        if nav == "2":
-            self.run()
-    def run(self):
+        print(f'Welcome to the main page!')
         while True:
-            self.cred()
-            self.log()
+            print("choose from the menu:")
+            nav = input("1)Register \n2)Login")
+            if nav == "1":
+                Register.reg()
+            if nav == "2":
+                self.user = Login.cred()
+                if self.user.check_cerd():
+                    break
 
 
+    def project_page(self):
+        while True:
+            print(f'\nWelcome to projects page {self.user.email}!')
+            print("choose from the menu:")
+            nav = input("1)Create new project \n2)View all projects \n3)View your projects \n3)edit your projects \n4)delete your projects")
+            if nav == "1":
+                Projects.prog(self.user.email)
+                print("project created successfully")
+            if nav == "2":
+                db_io.get_projects()
+                input("press any key when you are done")
+            if nav == "3":
+                db_io.get_user_projects(self.user.email)
+                input("press any key when you are done")
+            if nav == "4":
+                pass
+            if nav == "5":
+                d_title = input("whats the title of your project you want to delete")
+                db_io.delete_table(self.user.email, d_title)
 
+    def run(self):
+        self.start_page()
+        self.project_page()
 
-
-# usr1 = user('j','m','john@gmail.com','12345','2131231')
-#
-# print(usr1.f_name)
-
-# interface.start_page()
-
-interface().run()
+if __name__ == "__main__":
+    Interface().run()
 
